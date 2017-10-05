f266ba4ee63be682c23f533bccd0db61b90367d7
Andy Gaines <andyagaines@gmail.com>
2017-10-05 16:17:56 -0500

    finished main functionalities coding

--- weather.js
+++ weather.js
@@ -24,7 +24,11 @@ function render() {
   //add else if for 'view-three'
   if (STORE.view === 'view-two') {
     renderHTML();
-  }
+	}
+	
+	else if (STORE.view === 'view-three') {
+		renderNextHTML();
+	}
 }
 
 function getCurrentWeatherFromApi(){
@@ -38,31 +42,55 @@ function getCurrentWeatherFromApi(){
 
 function getForecastFromApi(){
   $.getJSON(`${API_CREDS.forecastEndpoint}q=${STORE.location},us&APPID=${apiKey}&units=imperial&dt=imperial`,(forecastData) => {
-  console.log(forecastData);
-});
+    STORE.forecast[0] = forecastData.list[7];
+    STORE.forecast[1] = forecastData.list[15];
+		STORE.forecast[2] = forecastData.list[23];
+		
+		render();
+  });
 }
 
 
 function renderHTML(){
 		
-  $('.results').html(`<div class="container day today">
+	$('.results').html(`<h2>Right Now</h2>
+		<div class="container day today">
 		<p>Current Temp: ${STORE.currentWeather.main.temp}</p>
 		<p>Hi: ${STORE.currentWeather.main.temp_max}</p>
 		<p>Lo: ${STORE.currentWeather.main.temp_min}</p>
 		<p> Whats the sky like?: ${STORE.currentWeather.weather[0].description}</p>
 	</div>
-  <button type='button' class='next'>Next 3 Days</button>
+	<button type='button' class='next'>Next 3 Days</button>
   `);
 }
 
+function renderNextHTML() {
 
+	$('.next-three').html(`<h2>Next Three Days</h2>
+	<div class="container day tomorrow">
+	<p>Hi: ${STORE.forecast[0].main.temp_max}</p>
+	<p>Lo: ${STORE.forecast[0].main.temp_min}</p>
+	<p>What's the sky like? ${STORE.forecast[0].weather[0].description}</p>
+	</div>
+	<br>
+	<div class="container day second-day">
+	<p>Hi: ${STORE.forecast[1].main.temp_max}</p>
+	<p>Lo: ${STORE.forecast[1].main.temp_min}</p>
+	<p>What's the sky like? ${STORE.forecast[0].weather[0].description}</p>
+	</div>
+	<br>
+	<div class="container day third-day">
+	<p>Hi: ${STORE.forecast[2].main.temp_max}</p>
+	<p>Lo: ${STORE.forecast[2].main.temp_min}</p>
+	<p>What's the sky like? ${STORE.forecast[0].weather[0].description}</p>
+	</div>`)
+}
 
 function watchNextClick(){
   $('.results').on('click', '.next', (event) => {
-    STORE.view = 'view-three';
-    getForecastFromApi()
-
-
+		STORE.view = 'view-three';
+		$('.next').hide();
+    getForecastFromApi();
   });
 }
 watchNextClick();
