c2b54358e294f57d762315269a52cf6ecbf836bd
edemahruh <dev@jesseparrott.com>
2017-10-06 11:17:56 -0500

    first iteration of app

--- weather.js
+++ weather.js
@@ -1,15 +1,15 @@
 'use strict';
-const baseApiUrl = 'http://api.openweathermap.org/data/2.5/';
-const weatherEndpoint = baseApiUrl + 'weather?';
-const forecastEndpoint = baseApiUrl + 'forecast?';
-const apiKey = 'a011671d71060d9062c408e8b9dc8ba9';
+function api_creds() {
+  this.baseApiUrl = 'http://api.openweathermap.org/data/2.5/';
+  this.weatherEndpoint = this.baseApiUrl + 'weather?';
+  this.forecastEndpoint = this.baseApiUrl + 'forecast?';
+  this.apiKey = 'a011671d71060d9062c408e8b9dc8ba9';
+  this.giphyEndpoint = 'http://api.giphy.com/v1/gifs/random?';
+  this.giphyApiKey = 'wXgyXTR1PI1IHHGcY0yv24Vz2UQkPOq4';
 
-const API_CREDS = {
-  baseApiUrl: 'http://api.openweathermap.org/data/2.5/',
-  weatherEndpoint: baseApiUrl + 'weather?',
-  forecastEndpoint: baseApiUrl + 'forecast?',
-  apiKey: 'a011671d71060d9062c408e8b9dc8ba9'
-};
+}
+
+const creds = new api_creds();
 
 const STORE = {
   view: 'initial',
@@ -20,19 +20,39 @@ const STORE = {
   ]
 };
 
+const backgrounds = {
+  sunny: '',
+  rain: '',
+  cloudy: ''
+};
+
 function render() {
   //add else if for 'view-three'
   if (STORE.view === 'view-two') {
     renderHTML();
-	}
+  }
 	
-	else if (STORE.view === 'view-three') {
-		renderNextHTML();
-	}
+  else if (STORE.view === 'view-three') {
+    renderNextHTML();
+  }
+}
+function getGiphyStuff(){
+  $.getJSON(`${creds.giphyEndpoint}api_key=${creds.giphyApiKey}&tag=Rainy`,(data) => {
+    backgrounds.rain = data.data.image_original_url;
+  });
+
+  $.getJSON(`${creds.giphyEndpoint}api_key=${creds.giphyApiKey}&tag=Sun`,(data) => {
+    backgrounds.sunny = data.data.image_original_url;
+  });
+
+  $.getJSON(`${creds.giphyEndpoint}api_key=${creds.giphyApiKey}&tag=Clouds`,(data) => {
+    backgrounds.cloudy = data.data.image_original_url;
+    console.log(backgrounds.cloudy);
+  });
 }
 
 function getCurrentWeatherFromApi(){
-  $.getJSON(`${API_CREDS.weatherEndpoint}q=${STORE.location},us&APPID=${apiKey}&units=imperial`,(data) => {
+  $.getJSON(`${creds.weatherEndpoint}q=${STORE.location},us&APPID=${creds.apiKey}&units=imperial`,(data) => {
     STORE.currentWeather = data;
     console.log(STORE.currentWeather);
 		
@@ -41,55 +61,105 @@ function getCurrentWeatherFromApi(){
 }
 
 function getForecastFromApi(){
-  $.getJSON(`${API_CREDS.forecastEndpoint}q=${STORE.location},us&APPID=${apiKey}&units=imperial&dt=imperial`,(forecastData) => {
+  $.getJSON(`${creds.forecastEndpoint}q=${STORE.location},us&APPID=${creds.apiKey}&units=imperial&dt=imperial`,(forecastData) => {
     STORE.forecast[0] = forecastData.list[7];
     STORE.forecast[1] = forecastData.list[15];
-		STORE.forecast[2] = forecastData.list[23];
+    STORE.forecast[2] = forecastData.list[23];
 		
-		render();
+    render();
   });
 }
 
 
 function renderHTML(){
 		
-	$('.results').html(`<h2>Right Now</h2>
+  $('.results').html(`<h2>Right Now</h2>
 		<div class="container day today">
-		<p>Current Temp: ${STORE.currentWeather.main.temp}</p>
-		<p>Hi: ${STORE.currentWeather.main.temp_max}</p>
-		<p>Lo: ${STORE.currentWeather.main.temp_min}</p>
-		<p> Whats the sky like?: ${STORE.currentWeather.weather[0].description}</p>
+		<p><span class="larger">${Math.round(STORE.currentWeather.main.temp)} °F</span></p>
+		<p class="conditions">${STORE.currentWeather.weather[0].description}</p>
 	</div>
 	<button type='button' class='next'>Next 3 Days</button>
-  `);
+	`);
+	
+  changeColor('.today', STORE.currentWeather);
 }
 
 function renderNextHTML() {
 
-	$('.next-three').html(`<h2>Next Three Days</h2>
+  $('.next-three').html(`<h2>Next Three Days</h2>
 	<div class="container day tomorrow">
-	<p>Hi: ${STORE.forecast[0].main.temp_max}</p>
-	<p>Lo: ${STORE.forecast[0].main.temp_min}</p>
-	<p>What's the sky like? ${STORE.forecast[0].weather[0].description}</p>
+	<p><span class="large">${Math.round(STORE.forecast[0].main.temp)} °F</span></p>
+	<p class="conditions">${STORE.forecast[0].weather[0].description}</p>
 	</div>
-	<br>
 	<div class="container day second-day">
-	<p>Hi: ${STORE.forecast[1].main.temp_max}</p>
-	<p>Lo: ${STORE.forecast[1].main.temp_min}</p>
-	<p>What's the sky like? ${STORE.forecast[0].weather[0].description}</p>
+	<p><span class="large">${Math.round(STORE.forecast[1].main.temp)} °F</span></p>
+	<p class="conditions">${STORE.forecast[1].weather[0].description}</p>
 	</div>
-	<br>
 	<div class="container day third-day">
-	<p>Hi: ${STORE.forecast[2].main.temp_max}</p>
-	<p>Lo: ${STORE.forecast[2].main.temp_min}</p>
-	<p>What's the sky like? ${STORE.forecast[0].weather[0].description}</p>
-	</div>`)
+	<p><span class="large">${Math.round(STORE.forecast[2].main.temp)} °F</span></p>
+	<p class="conditions">${STORE.forecast[2].weather[0].description}</p>
+	</div>`);
+
+  changeColor('.tomorrow', STORE.forecast[0]);
+  changeColor('.second-day', STORE.forecast[1]);
+  changeColor('.third-day', STORE.forecast[2]);
 }
 
+function changeColor(day, directory) {
+	
+  let num = directory.weather[0].id;
+
+  if (num < 300) {
+    $('body').css('background-image', `url('${backgrounds.rain}')`);
+    $(day).addClass('thunder');
+    $(day).append(`<div style="width:10%;height:0;padding-bottom:10%;position:relative;">
+		<iframe src="https://giphy.com/embed/VtOUGnwCOouCQ" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed">
+		</iframe></div>`);
+  }
+  else if (num >= 300 && num < 600) {
+    $('body').css('background-image', `url('${backgrounds.rain}')`);
+    $(day).addClass('rain');
+    $(day).append(`<div style="width:10%;height:0;padding-bottom:10%;position:relative;">
+		<iframe src="https://giphy.com/embed/3ov9jCEFMBtCy54q6Q" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed">
+		</iframe></div>`);
+  }
+  else if (num >= 600 && num < 700) {
+    $('body').css('background-image', `url('${backgrounds.cloudy}')`);
+    $(day).addClass('clouds-snow');
+    $(day).append(`<div style="width:10%;height:0;padding-bottom:10%;position:relative;">
+		<iframe src="https://giphy.com/embed/mBwNH39wXmJDq" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed">
+		</iframe></div>`);
+  }
+  else if (num >= 700 && num < 800) {
+    $('body').css('background-image', `url('${backgrounds.cloudy}')`);
+    $(day).addClass('fog');
+  }
+  else if (num === 800) {
+    $('body').css('background-image', `url('${backgrounds.sunny}')`);
+    $(day).addClass('clear');
+    $(day).append(`<div style="width:10%;height:0;padding-bottom:10%;position:relative;">
+		<iframe src="https://giphy.com/embed/xUPGcL7GsJ5398axOg" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed">
+		</iframe></div>`);
+  }
+  else if (num > 800 && num < 900) {
+    $(day).addClass('clouds-snow');
+    $(day).append(`<div style="width:10%;height:0;padding-bottom:10%;position:relative;">
+		<iframe src="https://giphy.com/embed/RHQtNg5bQ123C" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed">
+		</iframe></div>`);
+  }
+  else if (num >= 900 & num < 910) {
+    $(day).addClass('severe');
+    $(day).append(`<div style="width:10%;height:0;padding-bottom:10%;position:relative;">
+		<iframe src="https://giphy.com/embed/sN2N93vQwhsys" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed">
+		</iframe></div>`);
+  }
+}
+
+
 function watchNextClick(){
-  $('.results').on('click', '.next', (event) => {
-		STORE.view = 'view-three';
-		$('.next').hide();
+  $('.results').on('click', '.next', () => {
+    STORE.view = 'view-three';
+    $('.next').hide();
     getForecastFromApi();
   });
 }
@@ -114,7 +184,7 @@ function watchSubmit(){
     STORE.currentWeather = {};
     STORE.forecast = [];
     STORE.view = 'view-two';
-		
+    getGiphyStuff();
     getCurrentWeatherFromApi();
   });
 }
